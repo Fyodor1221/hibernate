8b6b7ea8f50a2f16754991b90f2c8fe24b1b8a92
Fyodor Parfyonov <parfyonov.fyodor@gmail.com>
2024-06-29 01:26:10 +0300

    Spring Security endpoints added

---
+++ src/main/java/ru/netology/hibernate/configuration/SecurityConfig.java
@@ -0,0 +1,30 @@
+package ru.netology.hibernate.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.formLogin(Customizer.withDefaults())
+                .authorizeHttpRequests(authz -> authz.requestMatchers("/persons/all").permitAll())
+                .authorizeHttpRequests(authz -> authz.anyRequest().authenticated());
+        return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("Fyodor")
+                .password("111")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+}
